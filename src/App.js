099a6da5f6f932dcb9da65a6e75280a49b524a7d
Lucas Melin <lucas.melin@gmail.com>
2020-07-05 10:59:03 -0400

    feat: add second slider for depression

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function App() {
-  const [ratings, setRatings] = useState([['rating a', 0], ['rating b', 2], ['rating c', 4]]);
+  const [ratings, setRatings] = useState([
+    { activity: "activity 1", anxiety: 0, depression: 0 },
+    { activity: "activity 2", anxiety: 35, depression: 50 },
+    { activity: "activity 3", anxiety: 20, depression: 80 },
+  ]);
 
   const handleSubmit = (rating) => {
     setRatings([...ratings, rating]);
@@ -25,15 +29,21 @@ function App() {
 }
 
 const SubmitForm = (props) => {
-  const [term, setTerm] = useState('');
-  const [rating, setRating] = useState(0);
+  const [term, setTerm] = useState("");
+  const [anxiety, setAnxiety] = useState(0);
+  const [depression, setDepression] = useState(0);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (term === '') return;
-    props.onFormSubmit([term, Number(rating)]);
-    setTerm('');
-    setRating(0);
+    if (term === "") return;
+    props.onFormSubmit({
+      activity: term,
+      anxiety: Number(anxiety),
+      depression: Number(depression),
+    });
+    setTerm("");
+    setAnxiety(0);
+    setDepression(0);
   };
 
   return (
@@ -51,8 +61,17 @@ const SubmitForm = (props) => {
         min="0"
         max="100"
         step="5"
-        value={rating}
-        onChange={(e) => setRating(e.target.value)}
+        value={anxiety}
+        onChange={(e) => setAnxiety(e.target.value)}
+      />
+      <input
+        type="range"
+        className="input"
+        min="0"
+        max="100"
+        step="5"
+        value={depression}
+        onChange={(e) => setDepression(e.target.value)}
       />
       <button className="button">Submit</button>
     </form>
@@ -61,33 +80,34 @@ const SubmitForm = (props) => {
 
 const Header = (props) => (
   <div>
-    <h1>
-      You have
-      {' '}
-      {props.numEntries}
-      {' '}
-      entries.
-    </h1>
+    <h1>You have{props.numEntries} entries. </h1>
   </div>
 );
 
 const EntriesList = (props) => {
-  const entries = props.entries.map((rating, index) => <Entry content={rating} key={index} id={index} onDelete={props.onDelete} />);
-  return (
-    <div>
-      {entries}
-    </div>
-  );
+  const entries = props.entries.map((rating, index) => (
+    <Entry content={rating} key={index} id={index} onDelete={props.onDelete} />
+  ));
+  return <div>{entries}</div>;
 };
 
-const Entry = (props) => {
-  console.log(props.content);
+const Entry = ({ content, onDelete, id }) => {
+  console.log(content);
   return (
     <div>
-      {props.content[0]}
-      {' - '}
-      {props.content[1]}
-      <button className="fa-times" onClick={() => { props.onDelete(props.id); }}>X</button>
+      {content.activity}
+      {" - Anxiety - "}
+      {content.anxiety}
+      {" - Depression - "}
+      {content.depression}
+      <button
+        className="fa-times"
+        onClick={() => {
+          onDelete(id);
+        }}
+      >
+        X
+      </button>
     </div>
   );
 };
