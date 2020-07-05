e639fbd13c72363461cbf9172b6aadf2fc9555d9
Lucas Melin <lucas.melin@gmail.com>
2020-07-05 14:16:27 -0400

    feat: restyle and humanize dates

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import moment from "moment";
 
 function retrieveObjectFromLocalStorage(item) {
   return JSON.parse(localStorage.getItem(item));
@@ -15,19 +16,19 @@ function App() {
         activity: "activity 1",
         anxiety: 0,
         depression: 0,
-        timestamp: Date(),
+        timestamp: moment(),
       },
       {
         activity: "activity 2",
         anxiety: 35,
         depression: 50,
-        timestamp: Date(),
+        timestamp: moment(),
       },
       {
         activity: "activity 3",
         anxiety: 20,
         depression: 80,
-        timestamp: Date(),
+        timestamp: moment(),
       },
     ]
   );
@@ -48,10 +49,13 @@ function App() {
 
   return (
     <div>
-      <div>
-        <Header numEntries={ratings.length} />
-        <EntriesList entries={ratings} onDelete={handleDelete} />
-        <SubmitForm onFormSubmit={handleSubmit} />
+      <Header />
+      <div className="mw9 center pa4 pt5-ns ph7-l">
+        <div>
+          <SubmitForm onFormSubmit={handleSubmit} />
+          <EntriesHeader numEntries={ratings.length} />
+          <EntriesList entries={ratings} onDelete={handleDelete} />
+        </div>
       </div>
     </div>
   );
@@ -69,7 +73,7 @@ const SubmitForm = (props) => {
       activity: term,
       anxiety: Number(anxiety),
       depression: Number(depression),
-      timestamp: Date(),
+      timestamp: moment(),
     });
     setTerm("");
     setAnxiety(0);
@@ -77,40 +81,65 @@ const SubmitForm = (props) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        className="input"
-        placeholder="Enter Item"
-        value={term}
-        onChange={(e) => setTerm(e.target.value)}
-      />
-      <input
-        type="range"
-        className="input"
-        min="0"
-        max="100"
-        step="5"
-        value={anxiety}
-        onChange={(e) => setAnxiety(e.target.value)}
-      />
-      <input
-        type="range"
-        className="input"
-        min="0"
-        max="100"
-        step="5"
-        value={depression}
-        onChange={(e) => setDepression(e.target.value)}
-      />
-      <button className="button">Submit</button>
+    <form className="bb" onSubmit={handleSubmit}>
+      <div>
+        <input
+          type="text"
+          className="input"
+          placeholder="What are you up to?"
+          value={term}
+          onChange={(e) => setTerm(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="anxiety" className="mt3 purple sans-serif b tl">
+          Anxiety
+        </label>
+        <input
+          type="range"
+          className="input"
+          id="anxiety"
+          min="0"
+          max="100"
+          step="5"
+          value={anxiety}
+          onChange={(e) => setAnxiety(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="depression" className="mt3 purple sans-serif b tl">
+          Depression
+        </label>
+        <input
+          type="range"
+          className="input"
+          min="0"
+          max="100"
+          step="5"
+          value={depression}
+          onChange={(e) => setDepression(e.target.value)}
+        />
+      </div>
+      <div>
+        <input
+          className="f6 link dim br3 ba ph3 pv2 mb2 dib purple"
+          type="submit"
+          value="Submit"
+        />
+      </div>
     </form>
   );
 };
 
-const Header = ({ numEntries }) => (
+const Header = () => (
+  <header className="bg-light-blue flex w-100 ph3 pv3 pv4-ns ph4-m ph5-l">
+    <div className="mt9 white sans-serif b tl f2">Togo</div>
+  </header>
+);
+
+const EntriesHeader = ({ numEntries }) => (
   <div>
-    <h1>You have {numEntries} entries. </h1>
+    <h1 className="f3 sans-serif">You have {numEntries} previous entries. </h1>
   </div>
 );
 
@@ -122,23 +151,23 @@ const EntriesList = ({ entries, onDelete }) => {
 };
 
 const Entry = ({ content, onDelete, id }) => {
+  const since = moment.duration(moment(content.timestamp).diff(moment()));
   return (
-    <div>
-      {content.activity}
-      {" - Anxiety - "}
-      {content.anxiety}
-      {" - Depression - "}
-      {content.depression}
-      {" - recorded at - "}
-      {content.timestamp.toString()}
-      <button
-        className="fa-times"
-        onClick={() => {
-          onDelete(id);
-        }}
-      >
-        X
-      </button>
+    <div className="sans-serif flex">
+      <div className="outline w-25 pa3 mr2">{content.activity}</div>
+      <div className="outline w-25 pa3 mr2">A{content.anxiety}</div>
+      <div className="outline w-25 pa3 mr2">D{content.depression}</div>
+      <div className="outline w-25 pa3 mr2">{since.humanize()} ago</div>
+      <div className="outline w-25 pa3 mr2">
+        <button
+          className="fa-times"
+          onClick={() => {
+            onDelete(id);
+          }}
+        >
+          X
+        </button>
+      </div>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import "./bootstrap.min.css";
+import "./css/tachyons.css";
 
 ReactDOM.render(
   <React.StrictMode>
